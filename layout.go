34573e64d8ccf1f75d6717ba56c54a2fed684b62
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-01 16:57:07 +0800

    add initLoadMode, initPEPath and initCommandLine to peLoader.

--- layout.go
+++ layout.go
@@ -2,6 +2,7 @@ package aurorium
 
 import (
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func (p *Program) initLayout() {
@@ -10,6 +11,14 @@ func (p *Program) initLayout() {
 	modules := container.NewGridWithColumns(3)
 	modules.Add(p.peLoader.Object())
 	grid.Add(modules)
+
+	configTpl := widget.NewSelect([]string{"default"}, func(s string) {
+
+	})
+	grid.Add(configTpl)
+
+	logger := widget.NewMultiLineEntry()
+	grid.Add(logger)
 
 	p.grid = grid
 }
--- pe_loader.go
+++ pe_loader.go
@@ -1,26 +1,74 @@
 package aurorium
 
 import (
+	"strings"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/lang"
 	"fyne.io/fyne/v2/widget"
 )
 
 type peLoader struct {
-	pePath *widget.Entry
+	grid *fyne.Container
+
+	mode string
+	path string
+	cmd  string
 }
 
 func newPELoader() *peLoader {
-	pePath := widget.NewEntry()
-	pePath.SetPlaceHolder(lang.L("pe_loader.pe_path.place_holder"))
-
 	loader := peLoader{
-		pePath: pePath,
+		grid: container.NewGridWithRows(4),
 	}
+	loader.initLoadMode()
+	loader.initPEPath()
+	loader.initCommandLine()
 	return &loader
 }
 
-func (ldr *peLoader) Object() fyne.CanvasObject {
+func (ldr *peLoader) initLoadMode() {
+	label := widget.NewLabel(lang.L("pe_loader.load_mode"))
 
-	return ldr.pePath
+	options := []string{"Embed", "HTTP", "File"}
+	modes := widget.NewRadioGroup(options, func(mode string) {
+		ldr.mode = strings.ToLower(mode)
+	})
+	modes.Required = true
+	modes.Horizontal = true
+	modes.SetSelected(options[0])
+
+	box := container.NewHBox(label, modes)
+	ldr.grid.Add(box)
+}
+
+func (ldr *peLoader) initPEPath() {
+	label := widget.NewLabel(lang.L("pe_loader.pe_image"))
+
+	path := widget.NewEntry()
+	path.OnChanged = func(path string) {
+		ldr.path = path
+	}
+	path.SetPlaceHolder(lang.L("pe_loader.pe_path.place_holder"))
+
+	grid := container.NewGridWithColumns(1, path)
+
+	box := container.NewHBox(label, grid)
+	ldr.grid.Add(box)
+}
+
+func (ldr *peLoader) initCommandLine() {
+	label := widget.NewLabel(lang.L("pe_loader.cmd_line"))
+
+	cmd := widget.NewEntry()
+	cmd.OnChanged = func(cmd string) {
+		ldr.cmd = cmd
+	}
+
+	box := container.NewHBox(label, cmd)
+	ldr.grid.Add(box)
+}
+
+func (ldr *peLoader) Object() fyne.CanvasObject {
+	return ldr.grid
 }
