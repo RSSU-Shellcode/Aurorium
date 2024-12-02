05164c05c89a66cae1f145453b0b422d58eb701d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-02 22:37:15 +0800

    update initTheme about load font, try to fix bug, improve Size().

--- theme.go
+++ theme.go
@@ -1,12 +1,18 @@
 package aurorium
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/theme"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/sfnt"
+	"golang.org/x/image/math/fixed"
 )
 
 type customTheme struct {
@@ -21,18 +27,39 @@ func (p *Program) initTheme() error {
 	t := customTheme{
 		Theme: theme.Current(),
 	}
-	for _, font := range []struct {
+	for _, item := range []struct {
 		name string
 		res  *fyne.Resource
 	}{
 		{"NotoSansSC-Regular.ttf", &t.regular},
 		{"NotoSansSC-Bold.ttf", &t.bold},
 	} {
-		data, err := os.ReadFile(filepath.Join("font", font.name))
+		data, err := os.ReadFile(filepath.Join("font", item.name))
 		if err != nil {
 			return err
 		}
-		*font.res = fyne.NewStaticResource(font.name, data)
+
+		f, err := sfnt.Parse(data)
+		if err != nil {
+			return err
+		}
+
+		buffer := &sfnt.Buffer{}
+
+		pem := fixed.Int26_6(f.UnitsPerEm())
+		got, err := f.Metrics(buffer, pem, font.HintingNone)
+		if err != nil {
+			return err
+		}
+		fmt.Println(got)
+
+		buf := bytes.NewBuffer(make([]byte, 0, len(data)))
+		_, err = f.WriteSourceTo(buffer, buf)
+		if err != nil {
+			return err
+		}
+
+		*item.res = fyne.NewStaticResource(item.name, buf.Bytes())
 	}
 	p.app.Settings().SetTheme(&t)
 	return nil
@@ -50,9 +77,15 @@ func (t *customTheme) Font(style fyne.TextStyle) fyne.Resource {
 }
 
 func (t *customTheme) Size(name fyne.ThemeSizeName) float32 {
+	// fmt.Println(name, t.Theme.Size(name))
+	switch {
 	// disable rounded corners style
-	if strings.Contains(strings.ToLower(string(name)), "radius") {
+	case strings.Contains(strings.ToLower(string(name)), "radius"):
 		return 0
+	// adjust general text size
+	case string(name) == "text":
+		return 13
+	default:
+		return t.Theme.Size(name)
 	}
-	return t.Theme.Size(name)
 }
