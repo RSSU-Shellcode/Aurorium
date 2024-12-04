298b7989127e1445be227a81ddf03c6c1784ab83
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-04 23:14:04 +0800

    add cmd/main.go for build GUI application program.

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
+	"os"
+
+	"github.com/RSSU-Shellcode/Aurorium"
 )
 
 func main() {
-
+	program := aurorium.New()
+	err := program.Main()
+	if err == nil {
+		return
+	}
+	_ = os.WriteFile("error.log", []byte(err.Error()), 0600)
 }
